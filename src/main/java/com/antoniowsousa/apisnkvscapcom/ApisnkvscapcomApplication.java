1d2846781cd0e9c3d14e0013d83e84fe1a3b792a
Antonio Sousa <antoniowsj@outlook.com>
2021-12-06 15:28:50 -0300

    Initial commit

---
+++ src/main/java/com/antoniowsousa/apisnkvscapcom/ApisnkvscapcomApplication.java
@@ -0,0 +1,14 @@
+package com.antoniowsousa.apisnkvscapcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisnkvscapcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisnkvscapcomApplication.class, args);
+		System.out.println("Aplicação no ar.");
+	}
+
+}
---
+++ src/test/java/com/antoniowsousa/apisnkvscapcom/ApisnkvscapcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniowsousa.apisnkvscapcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisnkvscapcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
